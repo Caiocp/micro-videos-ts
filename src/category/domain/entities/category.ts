a501dcdf30a84cd03c6f89b6aba6c37543371135
Caio Carvalho <ccarvalho451@gmail.com>
2022-08-19 00:11:57 +0000

    feat: :sparkles: added category entity

---
+++ src/category/domain/entities/category.ts
@@ -0,0 +1,42 @@
+import UniqueEntityId from '../../../shared/domain/value-objects/unique-entity-id.vo';
+
+export type CategoryProperties = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export class Category {
+  public readonly id: UniqueEntityId;
+  constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    this.id = id || new UniqueEntityId();
+    this.description = this.props.description;
+    this.props.is_active = this.props.is_active ?? true;
+    this.props.created_at = this.props.created_at ?? new Date();
+  }
+
+  get name() {
+    return this.props.name;
+  }
+
+  get description() {
+    return this.props.description;
+  }
+
+  private set description(description: string) {
+    this.props.description = description ?? null;
+  }
+
+  get is_active() {
+    return this.props.is_active ?? true;
+  }
+
+  private set is_active(is_active: boolean) {
+    this.props.is_active = is_active ?? null;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
+}
