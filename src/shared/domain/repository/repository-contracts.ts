2f45edd999a5f14e87ba4391689b3aa7be468370
Caio Carvalho <ccarvalho451@gmail.com>
2022-09-16 00:19:59 +0000

    feat: add search repository contract

--- src/shared/domain/repository/repository-contracts.ts
+++ src/shared/domain/repository/repository-contracts.ts
@@ -8,3 +8,154 @@ export interface RepositoryInterface<E extends Entity> {
   update(entity: E): Promise<void>;
   delete(id: string | UniqueEntityId): Promise<void>;
 }
+
+export type SortOrder = 'asc' | 'desc';
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  order?: SortOrder | null;
+  filter?: Filter | null;
+};
+export class SearchParams {
+  protected _page: number;
+  protected _per_page: number = 15;
+  protected _sort: string | null;
+  protected _order: SortOrder | null;
+  protected _filter: string | null;
+
+  constructor(props: SearchProps = {}) {
+    this.page = props.page;
+    this.per_page = props.per_page;
+    this.sort = props.sort;
+    this.order = props.order;
+    this.filter = props.filter;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  private set page(value: number) {
+    let _page = +value;
+
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1;
+    }
+
+    this._page = _page;
+  }
+
+  get per_page() {
+    return this._per_page;
+  }
+
+  private set per_page(value: number) {
+    let _per_page = value === (true as any) ? this._per_page : +value;
+
+    if (
+      Number.isNaN(_per_page) ||
+      _per_page <= 0 ||
+      parseInt(_per_page as any) !== _per_page
+    ) {
+      _per_page = this._per_page;
+    }
+
+    this._per_page = _per_page;
+  }
+
+  get sort(): string | null {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+
+  get order(): SortOrder | null {
+    return this._order;
+  }
+
+  private set order(value: string | null) {
+    if (!this.sort) {
+      this._order = null;
+      return;
+    }
+    const dir = `${value}`.toLowerCase();
+    this._order = dir !== 'asc' && dir !== 'desc' ? 'asc' : dir;
+  }
+
+  get filter() {
+    return this._filter;
+  }
+
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || (value as unknown) === ''
+        ? null
+        : (`${value}` as any);
+  }
+}
+
+type SearchResultProps<E extends Entity, Filter> = {
+  items: E[];
+  total: number;
+  current_page: number;
+  per_page: number;
+  sort: string | null;
+  order: string | null;
+  filter: Filter | null;
+};
+
+export class SearchResult<E extends Entity, Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly last_page: number;
+  readonly sort: string | null;
+  readonly order: string | null;
+  readonly filter: Filter;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = Math.ceil(props.total / props.per_page);
+    this.sort = props.sort;
+    this.order = props.order;
+    this.filter = props.filter;
+  }
+
+  toJSON() {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      last_page: this.last_page,
+      sort: this.sort,
+      order: this.order,
+      filter: this.filter,
+    };
+  }
+}
+
+/**
+ * SearchableRepositoryInterface is a repository that can be searched.
+ * It is used to search for entities in the repository.
+ * @Param E - The entity type.
+ * @Param P - The search input params type.
+ * @Param R - The search result type.
+ */
+export interface SearchableRepositoryInterface<
+  E extends Entity,
+  Filter = string,
+  P = SearchParams,
+  R = SearchResult<E, Filter>
+> extends RepositoryInterface<E> {
+  search(props: P): Promise<R>;
+}
