33e1e2b4993b0c884ce763a60b37ec66fd3e16c9
Caio Carvalho <ccarvalho451@gmail.com>
2022-10-12 22:21:24 +0000

    chore: set test threshold to 80% and add new script to package.json

--- jest.config.js
+++ jest.config.js
@@ -45,10 +45,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      statements: 95,
-      branches: 95,
-      functions: 95,
-      lines: 95,
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
     },
   },
 
